d7fe4c93d227a3ee6219d7f95ac5c666c74e1c4e
AxelRysjöLisslö <rysjolisslo.axel@elev.ga.ntig.se>
2022-09-07 12:29:58 +0200

    commit 2 togmuchi

--- togmuchi/togmuchi.cs
+++ togmuchi/togmuchi.cs
@@ -1,8 +1,19 @@
 using System;
-
-namespace .;
+using System.Collections.Generic;
 
 public class togmuchi
 {
     
+    private int hunger;
+    private List<string> ord = new List<string>() {"jak är hungrig"};
+    private bool Lever;
+    private Random generator;
+    public string namn;
+
+    public togmuchi()
+    { 
+        generator = new Random;
+        Lever = true;   
+    }
+
 }
