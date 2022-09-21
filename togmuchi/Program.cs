2520a1da63665ec24a9c8094969f5b6261bee9ef
AxelRysjöLisslö <rysjolisslo.axel@elev.ga.ntig.se>
2022-09-21 13:04:01 +0200

    last commit

--- togmuchi/Program.cs
+++ togmuchi/Program.cs
@@ -2,22 +2,37 @@
 
 togmuchi togmuchi = new togmuchi();
 
+Console.WriteLine("välj ett bra namn");
+togmuchi.namn = Console.ReadLine();
+
+
+
+Console.WriteLine($"{togmuchi.namn} är väll okej det finns bättre.");
 
 
 
 
+while ( togmuchi.Leva() == true)
+{
+    Console.Clear();
+    togmuchi.stats();
+    Console.WriteLine("Du måste mata den!");
+    Console.WriteLine($"1. Matar {togmuchi.namn}");
+    Console.WriteLine($"2. mata den inte {togmuchi.namn}");
 
+string val = Console.ReadLine();
+if (val == "1")
+{
+togmuchi.Mata();
+}
+else
+{
 
+Console.WriteLine("matar den inte");
+}
+togmuchi.tik();
+}
 
-
-
-
-
-
-
-
-
-
-
-
+Console.WriteLine($" bra du matade inte de nu är {togmuchi.namn} död");
+Console.WriteLine("tryck enter för att göra en ny");
 Console.ReadLine();
--- togmuchi/togmuchi.cs
+++ togmuchi/togmuchi.cs
@@ -51,7 +51,8 @@ public bool Leva()
 {
 
 return Lever;
-   
+
+    
 }
 
 
